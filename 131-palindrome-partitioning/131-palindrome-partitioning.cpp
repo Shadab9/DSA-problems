b5ee2ffda2e0f6be8486a2574af4ed4b2cac0b19
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-01-07 00:23:14 +0530

    Time: 249 ms (17.12%), Space: 57.5 MB (65.30%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int ispalin(string s,int l,int r)
+    {
+        while(l<=r)
+        {
+            if(s[l++]!=s[r--])
+                return 0;
+        }
+        return 1;
+    }
+    void help(string s,int st,vector<string>&path,vector<vector<string>>&result)
+    {
+        if(st==s.size())
+        {
+            result.push_back(path);
+            return;
+        }
+        
+        for(int i=st;i<s.size();i++)
+        {
+            if(ispalin(s,st,i))
+            {
+                path.push_back(s.substr(st,i-st+1));
+                help(s,i+1,path,result);
+                path.pop_back();
+            }
+        }
+        
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>result;
+        vector<string>path;
+        help(s,0,path,result);
+        return result;
+    }
+};
