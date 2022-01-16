0a7d853079e49ca7a19e9d8194dba71075b44277
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-01-17 02:15:20 +0530

    Time: 320 ms (21.18%), Space: 149.9 MB (20.02%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void combineUtil(vector<int>dp,int s,int n,int k,vector<int>& temp,vector<vector<int>>& result)
+    {
+        if(k==0)
+        {
+            result.push_back(temp);
+            return;
+        }
+        if(s==n)
+            return;
+        for(int i=s;i<n;i++)
+        {
+            temp.push_back(dp[i]);
+            combineUtil(dp,i+1,n,k-1,temp,result);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<vector<int>>result;
+        vector<int>dp(n);
+        for(int i=0;i<n;i++)
+            dp[i]=i+1;
+        
+        vector<int>temp;
+        combineUtil(dp,0,n,k,temp,result);
+        return result;
+        
+        
+    }
+};
