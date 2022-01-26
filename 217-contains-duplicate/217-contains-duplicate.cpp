8498a02ff599c2bf6592ce7515553edf61254769
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-01-26 14:47:13 +0530

    Time: 170 ms (17.82%), Space: 46.4 MB (76.39%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,11 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         if(nums.size()==0)
             return false;
-        unordered_map<int,int>freq;
-        for(int i=0;i<nums.size();i++)
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
         {
-            freq[nums[i]]++;
-            if(freq[nums[i]]>1)
+            if(nums[i]==nums[i+1])
                 return true;
         }
         return false;
