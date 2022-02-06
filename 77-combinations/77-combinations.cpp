1f12ed54d878d86a41c3d0fba3fc021023a8214e
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-06 19:05:05 +0530

    Time: 605 ms (12.79%), Space: 149.9 MB (19.80%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -14,6 +14,7 @@ public:
             temp.push_back(dp[i]);
             combineUtil(dp,i+1,n,k-1,temp,result);
             temp.pop_back();
+        
         }
     }
     vector<vector<int>> combine(int n, int k) {
