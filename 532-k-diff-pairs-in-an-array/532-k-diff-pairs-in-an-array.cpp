25a6d54fe872887e2be0765846bfd826bf10a8e6
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-09 12:22:37 +0530

    Time: 16 ms (86.74%), Space: 13.2 MB (58.82%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -4,18 +4,22 @@ public:
         unordered_map<int,int> m;
         int res=0;
         
-        for(int i : nums) {
-            if(m.find(i) == m.end()) {
+        for(int i : nums) 
+        {
+            if(m.find(i) == m.end())
+            {
                 if(m.find(i+k)!=m.end()) res++;
                 if(m.find(i-k)!=m.end()) res++;
                 m[i] = 1;
                 continue;
-            } else if(k == 0 && m[i]<=1) {
+            } 
+            else 
+            if(k == 0 && m[i]<=1)
+            {
                 res++;
             }
             m[i]++;
         }
-        
         return res;
     }
 };
