3eeb1cfd15f05a0725c65a001b7f2f314ea96bfa
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-10 14:08:20 +0530

    Time: 8 ms (74.83%), Space: 10.8 MB (89.09%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    void help(vector<int>&nums,int st,int target,vector<int>&temp,vector<vector<int>>&result)
+    void combinationUtil(vector<int>& candidates,int target,int start,vector<int>& temp,vector<vector<int>>& result)
     {
-        if(target==0 or st==nums.size())
+        if(start>=candidates.size() or target<=0)
         {
             if(target==0)
-            {
-                result.push_back(temp); 
-            }
+                result.push_back(temp);
             return;
         }
-        if(nums[st]<=target)
-        {
-            temp.push_back(nums[st]);
-            help(nums,st,target-nums[st],temp,result);
-            temp.pop_back();
-            help(nums,st+1,target,temp,result);
-        }
-        else
-        {
-            help(nums,st+1,target,temp,result);
-        }
+        
+        temp.push_back(candidates[start]);
+        combinationUtil(candidates,target-candidates[start],start,temp,result);
+        temp.pop_back();
+        combinationUtil(candidates,target,start+1,temp,result);
+        
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>result;
         vector<int>temp;
-        help(candidates,0,target,temp,result);
+        combinationUtil(candidates,target,0,temp,result);
         return result;
     }
 };
