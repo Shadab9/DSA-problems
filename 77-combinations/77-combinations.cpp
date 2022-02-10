36dc6061f55c966494e26291bcb6cd0ea483715e
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-10 14:02:31 +0530

    Time: 31 ms (62.94%), Space: 9.1 MB (79.09%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    void combineUtil(vector<int>dp,int s,int n,int k,vector<int>& temp,vector<vector<int>>& result)
+    void combineUtil(vector<int>& nums,int start,int k,vector<int>& temp,vector<vector<int>>& result)
     {
-        if(k==0)
+        
+        if( k==0 or start>=nums.size())
         {
+            if(k==0)
             result.push_back(temp);
             return;
         }
-        if(s==n)
-            return;
-        for(int i=s;i<n;i++)
-        {
-            temp.push_back(dp[i]);
-            combineUtil(dp,i+1,n,k-1,temp,result);
-            temp.pop_back();
-        
-        }
+        temp.push_back(nums[start]);
+        combineUtil(nums,start+1,k-1,temp,result);
+        temp.pop_back();
+        combineUtil(nums,start+1,k,temp,result);
     }
     vector<vector<int>> combine(int n, int k) {
-        
         vector<vector<int>>result;
-        vector<int>dp(n);
-        for(int i=0;i<n;i++)
-            dp[i]=i+1;
-        
+        vector<int>nums;
+        for(int i=1;i<=n;i++)
+            nums.push_back(i);
         vector<int>temp;
-        combineUtil(dp,0,n,k,temp,result);
+        combineUtil(nums,0,k,temp,result);
         return result;
-        
-        
     }
 };
