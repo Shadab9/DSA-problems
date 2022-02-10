555e0e6685b02982df4e7871a288e05418939c93
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-10 13:29:59 +0530

    Time: 697 ms (20.81%), Space: 31 MB (46.02%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    int longestUtil(vector<int>& nums,int start,int end)
+    {
+        unordered_map<int,int>elementFreq;
+        int answer=0;
+        for(int i=0;i<end;i++)
+        elementFreq[nums[i]]=1;
+        for(int i=0;i<end;i++)
+        {
+            if(elementFreq[nums[i]-1]==0)
+            {
+                int count=0;
+                int value=nums[i];
+                while(elementFreq[value]==1)
+                {
+                    value++;
+                    count++;
+                }
+                answer=max(answer,count);
+            }
+        }
+        return answer;
+    }
     int longestConsecutive(vector<int>& nums) {
         int size=nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<size;i++)
-            mp[nums[i]]=1;
-        int ans=0;
-        for(int i=0;i<size;i++)
-        {
-            if(mp[nums[i]-1]==1)
-                continue;
-            int count=0;
-            int value=nums[i];
-            while(mp[value]==1)
-            {
-                count++;
-                value+=1;
-            }
-            ans=max(ans,count);
-        }
-        return ans;
+        return longestUtil(nums,0,size);
     }
 };
