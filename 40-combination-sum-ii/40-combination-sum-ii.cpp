9ca2c05a4ecc320cbd8184e80b7049c49b5f9e2f
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-10 14:23:01 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (96.46%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    void combinationSum2Util(vector<int>& candidates,int target,vector<int>&temp,vector<vector<int>>&result,int s)
+    void combinationUtil(vector<int>& candidates,int start,int target,vector<int>& temp,vector<vector<int>>& result)
     {
-        if(target==0)
+        if(start>=candidates.size() or target==0)
         {
-            result.push_back(temp);
+            if(target==0)
+                result.push_back(temp);
             return;
         }
-        for(int i=s;i<candidates.size();i++)
+       for(int i=start;i<candidates.size();i++)
         {
-            if(i>s and candidates[i]==candidates[i-1])
-                continue;
-            if(candidates[i]>target)
-                break;
+           if(i>start and candidates[i]==candidates[i-1])
+               continue;
+           if(candidates[i]>target)
+               break;
             temp.push_back(candidates[i]);
-            combinationSum2Util(candidates,target-candidates[i],temp,result,i+1);
+            combinationUtil(candidates,i+1,target-candidates[i],temp,result);
             temp.pop_back();
         }
     }
@@ -22,10 +23,7 @@ public:
         vector<vector<int>>result;
         vector<int>temp;
         sort(candidates.begin(),candidates.end());
-        combinationSum2Util(candidates,target,temp,result,0);
+        combinationUtil(candidates,0,target,temp,result);
         return result;
-        
     }
-    
-
 };
