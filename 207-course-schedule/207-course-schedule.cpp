db60aefdcbec8c8fae762337bb9299511220a8e4
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-20 23:20:51 +0530

    Time: 36 ms (38.86%), Space: 14.6 MB (33.46%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
         vector<int>adjList[numCourses];
-        int count=0;
+        
         vector<int>inDegree(numCourses,0);
         for(auto it:prerequisites)
         {
             adjList[it[1]].push_back(it[0]);
         }
-        
         for(auto it:prerequisites)
-        {
             inDegree[it[0]]++;
-        }
         queue<int>q;
         for(int i=0;i<numCourses;i++)
         {
             if(inDegree[i]==0)
                 q.push(i);
         }
+        int cnt=0;
         while(!q.empty())
         {
             auto temp=q.front();
             q.pop();
-            count++;
+            cnt++;
             for(auto it:adjList[temp])
             {
                 inDegree[it]--;
@@ -32,9 +29,6 @@ public:
                     q.push(it);
             }
         }
-        if(count==numCourses)
-            return 1;
-        return 0;
-        
+        return cnt==numCourses;
     }
 };
