bedcc65c8730d299730afa09598d8fdf0214ad25
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-02-25 17:47:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (89.65%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -3,27 +3,11 @@
 
 class Solution {
 public:
-    int binarysearch(long int l,long int r)
-    {
-        int flag=0;
-        while(l<=r)
-        {
-            long int mid=(l+r)/2;
-            if(isBadVersion(mid))
-            {
-                flag=mid;
-                r=mid-1;
-            }
-            if(isBadVersion(mid)==false)
-                l=mid+1;
-        }
-        return flag;
-    }
+    
     int firstBadVersion(int n) {
         
         long int l=1;
         long int r=n;
-        //return binarysearch(l,r);
         int flag=0;
         while(l<=r)
         {
