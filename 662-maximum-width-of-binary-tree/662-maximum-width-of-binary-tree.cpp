c19eb1c629ff76ad1add3045da487bb4421ba9a8
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-01 11:13:44 +0530

    Time: 15 ms (42.49%), Space: 17.2 MB (96.52%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,43 +12,32 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        // queue<pair<TreeNode*,unsigned long long int>>q;
-        // q.push({root,0});
-        // int ans=0;
-        // while(!q.empty())
-        // {
-        //     int size=q.size();
-        //     int first,second;
-        //     for(int i=0;i<size;i++)
-        //     {
-        //         auto temp=q.front();
-        //         TreeNode* node=temp.first;
-        //         long long int tempval=temp.second;
-        //         q.pop();
-        //         if(i==0)
-        //             first=temp.second;
-        //         if(i==size-1)
-        //             second=temp.second;
-        //         if(node->left)
-        //             q.push({node->left,2*tempval*1ll+1});
-        //         if(node->right)
-        //             q.push({node->right,2*tempval*1ll+2});
-        //     }
-        //     ans=max(ans,second-first+1);
-        // }
-        // return ans;
-        long ans = 0; 
-        queue<pair<TreeNode*, long>> q; q.emplace(root, 0); 
-        while (q.size()) {
-            ans = max(ans, q.back().second - q.front().second + 1); 
-            int offset = q.front().second; 
-            for (int sz = q.size(); sz; --sz) {
-                auto [node, x] = q.front(); q.pop(); 
-                x -= offset; 
-                if (node->left) q.emplace(node->left, 2*x); 
-                if (node->right) q.emplace(node->right, 2*x+1); 
+        queue<pair<TreeNode*,unsigned long long int>>q;
+        q.push({root,0});
+        int ans=0;
+        while(!q.empty())
+        {
+            int size=q.size();
+            int first,second;
+            for(int i=0;i<size;i++)
+            {
+                auto temp=q.front();
+                TreeNode* node=temp.first;
+                 if(i==0)
+                    first=temp.second;
+                long long int tempval=temp.second-first;
+                q.pop();
+               
+                if(i==size-1)
+                    second=temp.second;
+                if(node->left)
+                    q.push({node->left,2*tempval*1ll+1});
+                if(node->right)
+                    q.push({node->right,2*tempval*1ll+2});
             }
+            ans=max(ans,second-first+1);
         }
-        return ans; 
+        return ans;
+       
     }
 };
