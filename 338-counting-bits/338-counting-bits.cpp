2785e340736ccaecf2f2bc64f2ffcba186c30d57
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-01 10:54:23 +0530

    Time: 6 ms (65.69%), Space: 8 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    void countBitsUtil(int n,vector<int>&result)
-    {
-        for(int i=0;i<=n;i++)
-        {
-            bitset<32>b1(i);
-            result.push_back(b1.count());
-        }
-    }
     vector<int> countBits(int n) {
         vector<int>result(n+1,0);
         
