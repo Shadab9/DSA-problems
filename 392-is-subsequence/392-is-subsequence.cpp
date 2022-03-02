43fb7c7cf066df35bc374d4255af129a78a91307
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-02 12:44:10 +0530

    Time: 2 ms (52.38%), Space: 6.5 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int s_size=s.size();
-        int t_size=t.size();
-        int dp[s_size+1][t_size+1];
-        for(int i=0;i<=s_size;i++)
+        // int s_size=s.size();
+        // int t_size=t.size();
+        // int dp[s_size+1][t_size+1];
+        // for(int i=0;i<=s_size;i++)
+        // {
+        //     for(int j=0;j<=t_size;j++)
+        //     {
+        //         if(i==0 or j==0)
+        //             dp[i][j]=0;
+        //         else
+        //         if(s[i-1]==t[j-1])
+        //             dp[i][j]=1+dp[i-1][j-1];
+        //         else
+        //             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        //     }
+        // }
+        // return s_size==dp[s_size][t_size];
+        int l=0;
+        int r=0;
+        while(l<s.length() and r<t.length())
         {
-            for(int j=0;j<=t_size;j++)
+            if(s[l]==t[r])
             {
-                if(i==0 or j==0)
-                    dp[i][j]=0;
-                else
-                if(s[i-1]==t[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                l++;
+                r++;
             }
+            else
+                r++;
         }
-        return s_size==dp[s_size][t_size];
+        if(l==s.length())return true;
+        return false;
     }
 };
