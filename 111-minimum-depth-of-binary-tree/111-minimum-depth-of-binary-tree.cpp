03cb8659eae14635fe4358e7926f606c036ad73e
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-25 16:32:27 +0530

    Time: 315 ms (65.13%), Space: 144.6 MB (71.97%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,28 +12,38 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-         if(root==nullptr)
+        // if(root==nullptr)
+        //     return 0;
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // int c=0;
+        // while(!q.empty())
+        // {
+        //     int n=q.size();
+        //     while(n>0)
+        //     {
+        //     TreeNode* temp=q.front();
+        //     if(temp->left==nullptr and temp->right==nullptr)
+        //         return ++c;
+        //     q.pop();
+        //     if(temp->left!=nullptr)
+        //     q.push(temp->left);
+        //     if(temp->right!=nullptr)
+        //     q.push(temp->right);
+        //         n--;
+        //     }
+        //     c++;
+        // }
+        // return c;
+        if(root==nullptr)
             return 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        int c=0;
-        while(!q.empty())
-        {
-            int n=q.size();
-            while(n>0)
-            {
-            TreeNode* temp=q.front();
-            if(temp->left==nullptr and temp->right==nullptr)
-                return ++c;
-            q.pop();
-            if(temp->left!=nullptr)
-            q.push(temp->left);
-            if(temp->right!=nullptr)
-            q.push(temp->right);
-                n--;
-            }
-            c++;
-        }
-        return c;
+        if(root->left==nullptr and root->right==nullptr)
+            return 1;
+        int ld=(root->left)?minDepth(root->left):INT_MAX;
+        int rd=(root->right)?minDepth(root->right):INT_MAX;
+        if(ld>rd)
+            return rd+1;
+        else
+            return ld+1;
     }
 };
