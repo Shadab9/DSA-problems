ab36ef815e5cb8d9c398475ebb40f05c0ccd5a83
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-27 01:01:42 +0530

    Time: 9 ms (6.12%), Space: 7.7 MB (76.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int robUtil(vector<int>&nums,int start,int end,vector<int>& dp)
+    int robUtil(vector<int>&nums,int start,int end,vector<int>&result)
     {
-        if(dp[start]!=-1)
-            return dp[start];
-        if(start>end)
+        if(result[start]!=-1)
+            return result[start];
+        if(start>=end)
+        {
             return 0;
-        
-        int op1=nums[start]+robUtil(nums,start+2,end,dp);
-        int op2=robUtil(nums,start+1,end,dp);
-        return dp[start]=max(op1,op2);
+        }
+        int opt1=nums[start]+robUtil(nums,start+2,end,result);
+        int opt2=robUtil(nums,start+1,end,result);
+        return result[start]=max(opt1,opt2);
+            
     }
     int rob(vector<int>& nums) {
-        vector<int>dp(400,-1);
+        vector<int>result(nums.size()+2,-1);
+        int sum=0;
+        return robUtil(nums,0,nums.size(),result);
         
-        return robUtil(nums,0,nums.size()-1,dp);
     }
 };
