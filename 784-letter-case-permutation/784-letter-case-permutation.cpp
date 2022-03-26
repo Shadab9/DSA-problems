2b2cca5fea83929e7fedd15f21b840ff5667da68
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-26 15:16:29 +0530

    Time: 16 ms (23.16%), Space: 11.4 MB (43.25%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    void letterUtil(string s,int start,vector<string>&result,string& temp)
+    void letterUtil(string s,int start,vector<string>&result,string &temp)
     {
+       
         if(start>=s.size())
         {
             result.push_back(temp);
@@ -13,7 +14,7 @@ public:
             letterUtil(s,start+1,result,temp);
             temp.pop_back();
             
-            s[start]=(s[start]>='a' and s[start]<='z'?toupper(s[start]):tolower(s[start]));
+            s[start]=s[start]>='a' and s[start]<='z'?toupper(s[start]):tolower(s[start]);
             temp.push_back(s[start]);
             letterUtil(s,start+1,result,temp);
             temp.pop_back();
@@ -25,10 +26,12 @@ public:
             temp.pop_back();
         }
     }
+        
     vector<string> letterCasePermutation(string s) {
         vector<string>result;
         string temp;
         letterUtil(s,0,result,temp);
+        
         return result;
     }
 };
