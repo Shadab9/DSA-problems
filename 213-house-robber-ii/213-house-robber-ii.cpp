15266fc4f57d1e19919e585fb4f7e2b79a2a2a67
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-27 01:11:31 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (60.15%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int dp[101];
-    int robUtil(vector<int>&nums,int start,int end)
+    int robUtil(vector<int>&nums,int start,int end,vector<int>&result)
     {
-        if(start>end)
+        
+        if(start>=end)
+        {
             return 0;
-        if(dp[start]!=-1)
-            return dp[start];
-        int first_case=nums[start]+robUtil(nums,start+2,end);
-        int second_case=robUtil(nums,start+1,end);
-        return dp[start]=max(first_case,second_case);
+        }
+        if(result[start]!=-1)
+            return result[start];
+        int opt1=nums[start]+robUtil(nums,start+2,end,result);
+        int opt2=robUtil(nums,start+1,end,result);
+        return result[start]=max(opt1,opt2);
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1)
             return nums[0];
-        memset(dp,-1,sizeof(dp));
-        int first_case=robUtil(nums,0,nums.size()-2);
-        memset(dp,-1,sizeof(dp));
-        int second_case=robUtil(nums,1,nums.size()-1);
-        return max(first_case,second_case);
+        vector<int>result(nums.size()+1,-1);
+        
+        int first_max=robUtil(nums,0,nums.size()-1,result);
+        vector<int>result1(nums.size()+1,-1);
+        int second_max=robUtil(nums,1,nums.size(),result1);
+        return max(first_max,second_max);
     }
 };
