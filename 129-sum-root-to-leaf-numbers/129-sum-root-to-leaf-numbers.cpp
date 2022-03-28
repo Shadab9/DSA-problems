6aec03dbc84fb43a597ec2c429bf907760e746b7
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-28 13:13:16 +0530

    Time: 3 ms (66.57%), Space: 9.2 MB (47.01%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -11,36 +11,28 @@
  */
 class Solution {
 public:
-    void sumUtil(TreeNode* root,vector<int>&temp,vector<vector<int>>&result)
+    
+    void sumNumbersUtil(TreeNode* root,int curr,int &ans)
     {
         if(root==nullptr)
             return;
         if(root->left==nullptr and root->right==nullptr)
         {
-            vector<int>base=temp;
-            base.push_back(root->val);
-            result.push_back(base);
+            curr=(curr*10)+(root->val);
+            ans+=curr;
             return;
         }
+        curr=(curr*10)+(root->val);
+        sumNumbersUtil(root->left,curr,ans);
         
-        temp.push_back(root->val);
-        sumUtil(root->left,temp,result);
-        sumUtil(root->right,temp,result);
-        temp.pop_back();
+        sumNumbersUtil(root->right,curr,ans);
+        
     }
     int sumNumbers(TreeNode* root) {
-        vector<vector<int>>result;
-        vector<int>temp;
-        sumUtil(root,temp,result);
-         int sum=0;
-        for(auto it:result)
-        {
-            string x="";
-            for(auto i:it)
-                x=x+to_string(i);
-            cout<<x<<endl;
-            sum+=stoi(x);
-        }
-        return sum;
+        
+       int curr=0,ans=0;
+        sumNumbersUtil(root,curr,ans);
+        
+        return ans;
     }
 };
