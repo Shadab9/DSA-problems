aa303a4e5c86acedba8294a3db58f2dbc44970b7
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-03-30 17:08:21 +0530

    Time: 112 ms (26.89%), Space: 28.8 MB (19.77%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    int check(vector<vector<int>>&matrix,int i,int j,vector<vector<int>>&result)
+    int check(vector<vector<int>>&matrix,int i,int j,int row,int col,vector<vector<int>>&result)
     {
         if(result[i][j]>0)
             return result[i][j];
-    int size=matrix.size();
-        int size1=matrix[0].size();
-    vector<int>dx={0,1,0,-1};
-    vector<int>dy={1,0,-1,0};
-    int amx=0;
-    for(int k=0;k<4;k++)
-    {
-        int new_X=dx[k]+i;
-        int new_Y=dy[k]+j;
-        if(new_X>=0 and new_X<size and new_Y>=0 and new_Y<size1 and matrix[new_X][new_Y]>matrix[i][j])
-        {
-            amx=max(amx,check(matrix,new_X,new_Y,result));
-        }
-    }
-    result[i][j]=amx+1;
-    return amx+1;
+        vector<int>dx={1,-1,0,0};
+        vector<int>dy={0,0,1,-1};
+        int count=0;
+            for(int k=0;k<4;k++)
+            {
+                int new_X=i+dx[k];
+                int new_Y=j+dy[k];
+                if(new_X>=0 and new_X<row and new_Y>=0 and new_Y<col and matrix[i][j]<matrix[new_X][new_Y])
+                {
+                    count=max(count,check(matrix,new_X,new_Y,row,col,result));
+                }
+            }
+        result[i][j]=count+1;
+        return count+1;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        
-        
-    int size=matrix.size();
-    int size1=matrix[0].size();
-    int mx=0;
-        vector<vector<int>>result(size,vector<int>(size1,0));
-    for(int i=0;i<size;i++)
-    {
-        for(int j=0;j<size1;j++)
+        int row=matrix.size(),col=matrix[0].size();
+        int ans=0;
+        vector<vector<int>>result(row,vector<int>(col,0));
+        for(int i=0;i<row;i++)
         {
-            mx=max(mx,check(matrix,i,j,result));
+            for(int j=0;j<col;j++)
+            {
+                ans=max(ans,check(matrix,i,j,row,col,result));
+            }
         }
-    }
-    return mx;
+        return ans;
+        
     }
 };
