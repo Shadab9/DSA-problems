34115879d01a1783f2480627d818480f842ead8c
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-05-10 23:50:20 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.34%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    
-    
     void combinationUtil(vector<int>&nums,int k,int n,int start,vector<int>&temp,vector<vector<int>>&result)
     {
-        
-        
         if((start>=nums.size() ) or (n==0 or k==0))
         {
             if(n==0 and k==0)
                 result.push_back(temp);
             return;
         }
-        // if(nums[start]<=n)
-        // {
-            temp.push_back(nums[start]);
-            combinationUtil(nums,k-1,n-nums[start],start+1,temp,result);
-            temp.pop_back();
-            combinationUtil(nums,k,n,start+1,temp,result);
-        
+        temp.push_back(nums[start]);
+        combinationUtil(nums,k-1,n-nums[start],start+1,temp,result);
+        temp.pop_back();
+        combinationUtil(nums,k,n,start+1,temp,result);
     }
         
     vector<vector<int>> combinationSum3(int k, int n) {
