bfdbd9e4ded036c1f6784caa1508ec6f10d2a520
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-05-12 00:59:37 +0530

    Time: 138 ms (6.30%), Space: 5.9 MB (85.48%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -8,16 +8,16 @@ public:
                 count++;
             return;
         }
-        //temp.push_back(vowels[ind]);
+        
         countUtil(vowels,ind,n-1,count);
-        //temp.pop_back();
+        
         countUtil(vowels,ind+1,n,count);
         
     }
     int countVowelStrings(int n) {
         vector<char> vowels={'a','e','i','o','u'};
         vector<char>temp;
-        //vector<vector<char>>result;
+       
         int count=0;
         countUtil(vowels,0,n,count);
         return count;
