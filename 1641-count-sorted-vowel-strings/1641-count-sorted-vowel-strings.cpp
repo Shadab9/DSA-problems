d3d3d3827f604745205b1f03845cf3af98f5c127
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-05-12 00:58:12 +0530

    Time: 390 ms (5.11%), Space: 6.1 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void countUtil(vector<char>&vowels,int ind,int n,vector<char>&temp,vector<vector<char>>&result)
+    void countUtil(vector<char>&vowels,int ind,int n,vector<char>&temp,int &count)
     {
         if(ind>=vowels.size() or n<=0)
         {
             if(n==0)
-                result.push_back(temp);
+                count++;
             return;
         }
         temp.push_back(vowels[ind]);
-        countUtil(vowels,ind,n-1,temp,result);
+        countUtil(vowels,ind,n-1,temp,count);
         temp.pop_back();
-        countUtil(vowels,ind+1,n,temp,result);
+        countUtil(vowels,ind+1,n,temp,count);
         
     }
     int countVowelStrings(int n) {
         vector<char> vowels={'a','e','i','o','u'};
         vector<char>temp;
-        vector<vector<char>>result;
-        countUtil(vowels,0,n,temp,result);
-        return result.size();
+        //vector<vector<char>>result;
+        int count=0;
+        countUtil(vowels,0,n,temp,count);
+        return count;
     }
 };
