269f69f765a6ab213311d54771d61a6e6d2e5900
Shadab9 <52657238+Shadab9@users.noreply.github.com>
2022-06-19 17:06:09 +0530

    Time: 185 ms (24.24%), Space: 29.4 MB (54.60%) - LeetHub

---
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> allPossibleBSTUtil(int r,unordered_map<int,vector<TreeNode*>>&mp)
+    {
+        vector<TreeNode*>result;
+       
+        if(mp.find(r)!=mp.end())
+            return mp[r];
+        if(r==1)
+        {
+            result.push_back(new TreeNode(0));
+            mp[r]=result;
+            return result;
+        }
+        for(int i=1;i<r;i++)
+        {
+            vector<TreeNode*>leftTree=allPossibleBSTUtil(i,mp);
+            vector<TreeNode*>rightTree=allPossibleBSTUtil(r-i-1,mp);
+            for(auto it:leftTree)
+            {
+                for(auto it1:rightTree)
+                {
+                    TreeNode* root=new TreeNode(0);
+                    root->left=it;
+                    root->right=it1;
+                    // if((it==nullptr and it1==nullptr ) or(it!=nullptr and it1!=nullptr))
+                    result.push_back(root);
+                }
+            }
+        }
+        return mp[r]=result;
+    }
+    
+    vector<TreeNode*> allPossibleFBT(int n) {
+        unordered_map<int,vector<TreeNode*>>mp;
+        vector<TreeNode*>result;
+        return allPossibleBSTUtil(n,mp);
+        
+    }
+};
